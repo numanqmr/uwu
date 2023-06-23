075089fd797de44290f28f1558ce17c8c0585fce
waleedbinalim <waleedbinalim@gmail.com>
2023-06-23 18:22:00 +0500

    chore: dynamic routing uwu

---
+++ pages/anime/[id].vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <NuxtLayout name="auth-layout">
+      <h1 class="text-lg dark:text-white">{{ route.params.id }}</h1>
+    </NuxtLayout>
+  </div>
+</template>
+
+<script setup>
+const route = useRoute();
+</script>
