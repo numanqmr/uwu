c0e2dbc68a94b67a37f3f358bf69ce59b9c32796
waleedbinalim <waleedbinalim@gmail.com>
2023-06-23 17:22:49 +0500

    chore: pagination row, personcard ui fix uwu

---
+++ components/pagination/PaginationRow.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="w-full flex justify-center items-center">
+    <div
+      class="cursor-pointer w-8 h-8 border-stone-600 bg-transparent hover:bg-stone-200 border-2 text-center dark:text-white font-semibold rounded-md align-middle"
+    >
+      <div class="grid items-center">
+        <p>&lt;</p>
+      </div>
+    </div>
+
+    <div
+      v-for="index in 8"
+      :key="index"
+      class="cursor-pointer w-8 h-8 border-stone-600 bg-transparent hover:bg-stone-200 border-2 text-center dark:text-white font-semibold rounded-md align-middle"
+    >
+      <div class="grid items-center">
+        <p>
+          {{ index }}
+        </p>
+      </div>
+    </div>
+
+    <div
+      class="cursor-pointer w-8 h-8 border-stone-600 bg-transparent hover:bg-stone-200 border-2 text-center dark:text-white font-semibold rounded-md align-middle"
+    >
+      <div class="grid items-center">
+        <p>></p>
+      </div>
+    </div>
+  </div>
+</template>
--- pages/people.vue
+++ pages/people.vue
@@ -7,9 +7,12 @@
       <div
         class="w-100 grid gap-4 grid-cols-2 grid-rows-2 sm:grid-cols-3 sm:grid-rows-3"
       >
-        <div v-for="index in 20" :key="index" class="mb-12 mx-4">
+        <div v-for="index in 20" :key="index" class="mb-12 mx-2">
           <PersonCard />
         </div>
+      </div>
+      <div class="pb-4">
+        <PaginationRow />
       </div>
     </NuxtLayout>
   </div>
