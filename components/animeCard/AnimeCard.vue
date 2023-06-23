c3e14288a187972e8d763806ff6f0202531ffde1
waleedbinalim <waleedbinalim@gmail.com>
2023-06-23 18:12:13 +0500

    i tried something uwu

--- components/animeCard/AnimeCard.vue
+++ components/animeCard/AnimeCard.vue
@@ -1,16 +1,15 @@
 <template>
-  <div
-    class="relative shadow-2xl max-w-sm rounded-xl overflow-hidden h-[500px] group"
-  >
-    <div class="h-[45%] overflow-hidden">
+  <div class="relative shadow-2xl w-1/4 rounded-xl overflow-hidden group">
+    <div class="h-44 overflow-hidden">
       <img
         src="https://cdn.myanimelist.net/images/anime/10/18793.jpg"
         alt="Desert"
         class="w-full h-full object-cover transition group-hover:duration-300 duration-700 group-hover:scale-110"
       />
     </div>
-    <div class="bg-gray-700 px-2 py-2 flex flex-col h-[55%]">
-      <div class="flex justify-center space-x-2">
+
+    <div class="bg-gray-700 px-2 py-2 flex flex-col">
+      <div class="flex justify-center space-x-2 flex-wrap w-full">
         <span
           key="{tag}"
           className="bg-blue-500 rounded-full text-gray-200 bg-opacity-60 text-sm px-2 py-[2px] font-bold"
@@ -18,7 +17,8 @@
           business
         </span>
       </div>
-      <div class="my-2 text-center">
+
+      <div class="my-2 text-center w-full">
         <h6 class="bg-clip-text text-[15px] text-transparent">
           <!-- class={clsx("bg-clip-text text-[15px] text-transparent", BGColor)} -->
           2023
@@ -28,7 +28,8 @@
           lorem uwu ipsum lorem uwu ipsum lorem uwu ipsum lorem uwu ipsum
         </p>
       </div>
-      <div class="flex space-x-4 mt-auto">
+
+      <div class="flex justify-evenly mt-2 w-full">
         <div
           class="flex flex-col flex-1 items-center text-gray-200 rounded-xl py-1 px-8 cursor-pointer"
         >
--- pages/anime.vue
+++ pages/anime.vue
@@ -2,7 +2,12 @@
   <div>
     <NuxtLayout name="auth-layout">
       <div class="text-xl my-14 font-extrabold dark:text-white">Anime List</div>
-      <AnimeCard />
+      <div class="flex flex-wrap gap-y-8 gap-x-1 justify-around">
+        <AnimeCard v-for="index in 15" :key="`anime-${index}`" />
+      </div>
+      <div class="py-4">
+        <PaginationRow />
+      </div>
     </NuxtLayout>
   </div>
 </template>
