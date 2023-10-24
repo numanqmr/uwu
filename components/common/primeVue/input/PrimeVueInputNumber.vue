985c734a1f9dbe3d0a2e2ecc8d76e6f23949ed96
waleedbinalim <waleedbinalim@gmail.com>
2023-10-24 20:52:25 +0500

    styling can be a pain in the butt

--- components/common/primeVue/input/PrimeVueInputNumber.vue
+++ components/common/primeVue/input/PrimeVueInputNumber.vue
@@ -15,8 +15,8 @@ const primeVueInputNumberStyle = {
   root: "w-full inline-flex",
   input: ({ props }) => ({
     class: [
-      "mb-4 block w-full rounded-lg border-2 border-blue-300 p-2.5 text-sm text-gray-900 shadow-sm",
-      "focus:shadow-none focus:border-blue-500 focus:outline-0 focus:outline-none",
+      "mb-4 block w-full rounded-lg border-2 !border-blue-200 p-[10px] text-sm text-gray-900 shadow-sm",
+      "focus:shadow-none focus:!border-blue-500 focus:outline-0 focus:outline-none",
       "dark:border-gray-600 dark:!bg-gray-700",
       "dark:focus:shadow-none dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500",
     ],
--- pages/add-anime/index.vue
+++ pages/add-anime/index.vue
@@ -14,72 +14,75 @@
       </p>
 
       <form @submit.prevent="handleSubmit">
-        <div class="grid grid-cols-2 gap-4 gap-x-8">
-          <div v-for="inputs in inputsList" :key="inputs.label" class="w-full">
-            <label
-              for="title"
-              class="block pb-2 text-sm font-bold text-gray-800 dark:text-white"
-              >{{ inputs.label }} :
-            </label>
+        <div class="grid gap-4 gap-x-8 md:grid-cols-2">
+          <div>
+            <div v-for="inputs in inputsList" :key="inputs.label">
+              <label
+                for="title"
+                class="block pb-2 text-sm font-bold text-gray-800 dark:text-white"
+                >{{ inputs.label }} :
+              </label>
 
-            <input
-              :type="inputs.inputType"
-              v-model="inputs.value"
-              class="mb-4 block w-full rounded-lg border-2 border-blue-200 p-2.5 text-sm text-gray-900 shadow-sm outline-none focus:border-blue-500 focus:ring-blue-500 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500"
-            />
+              <input
+                :type="inputs.inputType"
+                v-model="inputs.value"
+                class="mb-4 block w-full rounded-lg border-2 border-blue-200 p-2.5 text-sm text-gray-900 shadow-sm outline-none focus:border-blue-500 focus:ring-blue-500 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500"
+              />
+            </div>
+          </div>
+
+          <div>
+            <div class="flex gap-2">
+              <div class="w-1/4">
+                <label
+                  for="title"
+                  class="block pb-2 text-sm font-bold text-gray-800 dark:text-white"
+                  >Seasons
+                </label>
+                <PrimeVueInputNumber />
+              </div>
+              <div class="w-1/4">
+                <label
+                  for="title"
+                  class="block pb-2 text-sm font-bold text-gray-800 dark:text-white"
+                  >Episodes
+                </label>
+                <PrimeVueInputNumber />
+              </div>
+              <div class="w-1/2">
+                <label
+                  for="title"
+                  class="block pb-2 text-sm font-bold text-gray-800 dark:text-white"
+                  >Year
+                </label>
+                <PrimeVueInputNumber />
+              </div>
+            </div>
+            <div class="flex-col">
+              <label
+                for="title"
+                class="block pb-2 text-sm font-bold text-gray-800 dark:text-white"
+                >Status
+              </label>
+              <PrimeVueDropdown placeholder="Select Status" />
+            </div>
+
+            <div class="flex-col">
+              <label
+                for="title"
+                class="block pb-2 text-sm font-bold text-gray-800 dark:text-white"
+                >Genres
+              </label>
+
+              <PVMultiSelect
+                :options="listedAnimeGenres"
+                placeholder="Select Genre(s)"
+                :selectionLimit="3"
+              />
+            </div>
           </div>
         </div>
 
-        <div class="flex w-1/2 gap-2 pr-4">
-          <div class="w-1/4 flex-col">
-            <label
-              for="title"
-              class="block pb-2 text-sm font-bold text-gray-800 dark:text-white"
-              >Seasons
-            </label>
-            <PrimeVueInputNumber />
-          </div>
-          <div class="w-1/4 flex-col">
-            <label
-              for="title"
-              class="block pb-2 text-sm font-bold text-gray-800 dark:text-white"
-              >Episodes
-            </label>
-            <PrimeVueInputNumber />
-          </div>
-          <div class="w-1/2 flex-col">
-            <label
-              for="title"
-              class="block pb-2 text-sm font-bold text-gray-800 dark:text-white"
-              >Year Released
-            </label>
-            <PrimeVueInputNumber />
-          </div>
-        </div>
-        <div class="w-1/2 flex-col">
-          <label
-            for="title"
-            class="block pb-2 text-sm font-bold text-gray-800 dark:text-white"
-            >Status
-          </label>
-          <div class="pr-4">
-            <PrimeVueDropdown placeholder="Select Status" />
-          </div>
-        </div>
-        <div class="w-1/2 flex-col">
-          <label
-            for="title"
-            class="block pb-2 text-sm font-bold text-gray-800 dark:text-white"
-            >Genres
-          </label>
-          <div class="pr-4">
-            <PVMultiSelect
-              :options="listedAnimeGenres"
-              placeholder="Select Genre(s)"
-              :selectionLimit="3"
-            />
-          </div>
-        </div>
         <h3 class="pb-4 text-2xl font-bold dark:text-white">Description:</h3>
 
         <TextEditor @text-update="(text) => updateText(text)" />
