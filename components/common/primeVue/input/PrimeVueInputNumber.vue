8ae4b4a1ff53c60a33d4b213156bc4bc992471b9
waleedbinalim <waleedbinalim@gmail.com>
2023-10-24 19:43:04 +0500

    chore: minor css annoyances, more to come

--- components/common/primeVue/input/PrimeVueInputNumber.vue
+++ components/common/primeVue/input/PrimeVueInputNumber.vue
@@ -13,17 +13,13 @@ const value = ref();
 
 const primeVueInputNumberStyle = {
   root: "w-full inline-flex",
-  input: {
-    class:
-      "mb-4 block w-full rounded-lg border-2 border-blue-200 p-2.5 text-sm text-gray-900 shadow-sm outline-none focus:border-blue-500 focus:ring-blue-500 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500",
-  },
-  buttongroup: ({ props }) => ({
+  input: ({ props }) => ({
     class: [
-      {
-        "flex flex-col": props.showButtons && props.buttonLayout == "stacked",
-      },
+      "mb-4 block w-full rounded-lg border-2 border-blue-300 p-2.5 text-sm text-gray-900 shadow-sm",
+      "focus:shadow-none focus:border-blue-500 focus:outline-0 focus:outline-none",
+      "dark:border-gray-600 dark:!bg-gray-700",
+      "dark:focus:shadow-none dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500",
     ],
   }),
-  label: "hidden",
 };
 </script>
--- components/common/primeVue/multi-select/PVMultiSelect.vue
+++ components/common/primeVue/multi-select/PVMultiSelect.vue
@@ -7,52 +7,19 @@
     :maxSelectedLabels="3"
     :selectionLimit="3"
     :showToggleAll="false"
-    :pt="{
-      root: ({ props }) => ({
-        class: [
-          'w-full text-sm shadow-sm inline-flex cursor-pointer select-none bg-white dark:bg-gray-700 border-2 border-blue-200 dark:border-gray-600  transition-colors duration-200 ease-in-out rounded-md',
-          {
-            'opacity-60 select-none pointer-events-none cursor-default':
-              props?.disabled,
-          },
-        ],
-      }),
-      label: { class: 'dark:text-white p-2' },
-      item: ({ context }) => ({
-        class: [
-          'cursor-pointer font-normal overflow-hidden relative whitespace-nowrap',
-          'm-0 p-3 border-0  transition-shadow duration-200 rounded-none',
-          {
-            'text-white-700 hover:text-gray-400 hover:bg-gray-200 dark:text-white dark:hover:bg-gray-800':
-              !context.focused && !context.selected,
-            'bg-gray-300 text-gray-700 dark:text-white dark:bg-gray-800/90 hover:text-gray-700 hover:bg-gray-200 dark:text-white dark:hover:bg-gray-800':
-              context.focused && !context.selected,
-            'bg-blue-100 text-blue-700 dark:bg-gray-600 dark:text-white':
-              context.focused && context.selected,
-            'bg-blue-50 text-white-700 dark:bg-gray-600 dark:text-white':
-              !context.focused && context.selected,
-          },
-        ],
-      }),
-
-      checkbox: ({ context }) => ({
-        class: [
-          'flex items-center justify-center',
-          'border-2 w-6 h-6 text-gray-600 dark:text-white rounded-lg transition-colors duration-200',
-          'hover:border-green-500 focus:outline-none focus:outline-offset-0 focus:shadow-[0_0_0_0.2rem_rgba(191,219,254,1)] dark:focus:shadow-[0_0_0_0.2rem_rgba(147,197,253,0.5)]',
-          {
-            'border-gray-300 dark:border-blue-900/40  bg-white dark:bg-gray-900':
-              !context?.selected,
-            'border-green-500 bg-green-500': context?.selected,
-          },
-        ],
-      }),
-    }"
+    @show="handleShow"
+    @hide="handleShow"
+    :pt="PVMultiSelectStyles"
   />
 </template>
 
 <script setup>
 import MultiSelect from "primevue/multiselect";
+const isOpen = ref(false);
+
+const handleShow = () => {
+  isOpen.value = !isOpen.value;
+};
 
 const selectedCities = ref();
 const cities = ref([
@@ -62,4 +29,50 @@ const cities = ref([
   { name: "Slice of Life", code: "Slice of Life" },
   { name: "Adventure", code: "Adventure" },
 ]);
+
+const PVMultiSelectStyles = {
+  root: ({ props }) => ({
+    class: [
+      "w-full text-sm shadow-sm inline-flex cursor-pointer select-none bg-white dark:bg-gray-700 border-2 rounded-md",
+      "active:!border-blue-500",
+      {
+        "opacity-60 select-none pointer-events-none cursor-default":
+          props?.disabled,
+      },
+      { "border-blue-500 dark:border-blue-500": isOpen.value },
+      { "border-blue-200 dark:border-gray-600": !isOpen.value },
+    ],
+  }),
+
+  label: { class: "dark:text-white p-2" },
+  item: ({ context }) => ({
+    class: [
+      "cursor-pointer font-normal overflow-hidden relative whitespace-nowrap",
+      "m-0 p-3 border-0  transition-shadow duration-200 rounded-none",
+      {
+        "text-white-700 hover:text-gray-400 hover:bg-gray-200 dark:text-white dark:hover:bg-gray-800":
+          !context.focused && !context.selected,
+        "bg-gray-300 text-gray-700 dark:text-white dark:bg-gray-800/90 hover:text-gray-700 hover:bg-gray-200 dark:text-white dark:hover:bg-gray-800":
+          context.focused && !context.selected,
+        "bg-blue-100 text-blue-700 dark:bg-gray-600 dark:text-white":
+          context.focused && context.selected,
+        "bg-blue-50 text-white-700 dark:bg-gray-600 dark:text-white":
+          !context.focused && context.selected,
+      },
+    ],
+  }),
+
+  checkbox: ({ context }) => ({
+    class: [
+      "flex items-center justify-center",
+      "border-2 w-6 h-6 text-gray-600 dark:text-white rounded-lg transition-colors duration-200",
+      "hover:border-green-500 focus:outline-none focus:outline-offset-0 focus:shadow-[0_0_0_0.2rem_rgba(191,219,254,1)] dark:focus:shadow-[0_0_0_0.2rem_rgba(147,197,253,0.5)]",
+      {
+        "border-gray-300 dark:border-blue-900/40  bg-white dark:bg-gray-900":
+          !context?.selected,
+        "border-green-500 bg-green-500": context?.selected,
+      },
+    ],
+  }),
+};
 </script>
