f63febfb5c19c76b52aaf1053fdee9005c7f2d5c
waleedbinalim <waleedbinalim@gmail.com>
2023-10-26 19:24:17 +0500

    chore: reviews page

---
+++ components/people/PeopleHeader.vue
@@ -0,0 +1,35 @@
+<template>
+  <div
+    class="relative h-64 w-full rounded-3xl bg-gradient-to-r from-green-100 from-10% via-green-200 via-30% to-emerald-500 to-90%"
+  >
+    <div
+      class="-clip-padding absolute bottom-[-20%] left-[50%] min-h-[32px] w-[85%] translate-x-[-50%] rounded-3xl border bg-gray-100 bg-opacity-60 drop-shadow-2xl backdrop-blur-sm backdrop-filter"
+    >
+      <div class="flex justify-center p-8">
+        <div class="flex flex-col items-center">
+          <div>
+            <img
+              class="h-24 w-24 rounded-full"
+              :src="getImgSrc(profilePic)"
+              alt="Rounded avatar"
+            />
+          </div>
+          <div class="ml-2 p-2">
+            <p class="text-lg font-bold">{{ name }}</p>
+            <p class="text-sm text-slate-600">
+              {{ email || "email@email.com" }}
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+const props = defineProps(["name", "email", "profilePic"]);
+
+const getImgSrc = (url) => {
+  if (url) return url;
+};
+</script>
--- pages/people/[id]/index.vue
+++ pages/people/[id]/index.vue
@@ -2,31 +2,11 @@
   <div>
     <NuxtLayout :name="`auth-layout`">
       <div class="px-4 pt-8">
-        <div
-          class="relative h-64 w-full rounded-3xl bg-gradient-to-r from-green-100 from-10% via-green-200 via-30% to-emerald-500 to-90%"
-        >
-          <div
-            class="-clip-padding absolute bottom-[-20%] left-[50%] min-h-[32px] w-[85%] translate-x-[-50%] rounded-3xl border bg-gray-100 bg-opacity-60 drop-shadow-2xl backdrop-blur-sm backdrop-filter"
-          >
-            <div class="flex justify-center p-8">
-              <div class="flex flex-col items-center">
-                <div>
-                  <img
-                    class="h-24 w-24 rounded-full"
-                    :src="getImgSrc(userData?.imgURL)"
-                    alt="Rounded avatar"
-                  />
-                </div>
-                <div class="ml-2 p-2">
-                  <p class="text-lg font-bold">{{ userData?.name }}</p>
-                  <p class="text-sm text-slate-600">
-                    {{ userData?.email || "email@email.com" }}
-                  </p>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
+        <PeopleHeader
+          :name="userData.name"
+          :email="userData.email"
+          :profilePic="userData.imgURL"
+        />
 
         <div class="mt-20 flex flex-col gap-4 md:flex-row">
           <div
@@ -134,6 +114,7 @@
 </template>
 
 <script setup>
+const router = useRouter();
 const route = useRoute();
 const id = route.params.id;
 const { data: userData } = await useFetch(`/api/people/${id}`, {
