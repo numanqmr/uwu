215c0837ef2fe074288d6cb33cb46bc4e7cf77f2
waleedbinalim <waleedbinalim@gmail.com>
2023-12-19 14:42:38 +0500

    chore: removed console log

--- styleguide/prime-vue-styles.ts
+++ styleguide/prime-vue-styles.ts
@@ -34,8 +34,7 @@ export const primeVueDropdownStyled = {
       "mt-2 max-h-[200px] overflow-auto bg-white text-gray-700 border-0 rounded-md shadow-lg dark:bg-gray-800 dark:text-white/80",
     ],
   },
-  item: ({ context }) => {
-    console.log(context);
+  item: ({ context }: any) => {
     return {
       class: [
         !context.selected
