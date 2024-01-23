224b7683262d281fc71db0719ca45b485004847a
waleedbinalim <waleedbinalim@gmail.com>
2024-01-23 13:28:07 +0500

    chore: added loader

--- app.vue
+++ app.vue
@@ -1,5 +1,16 @@
 <template>
   <div class="dark:bg-slate-900">
+    <div
+      v-if="!!loader.isLoading.value"
+      class="fixed bottom-0 right-0 z-50 mb-2 mr-2 grid h-16 w-16 place-content-center rounded-lg bg-blue-500"
+    >
+      <p class="font-bolder animate-spin uppercase text-white">I</p>
+    </div>
+
     <NuxtPage> </NuxtPage>
   </div>
 </template>
+
+<script setup lang="ts">
+const loader = useLoadingIndicator();
+</script>
