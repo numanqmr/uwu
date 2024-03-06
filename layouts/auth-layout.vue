88f35d8a8c3d3f43058eddd1c7a027e0610a64c2
waleedbinalim <waleedbinalim@gmail.com>
2024-03-06 17:02:45 +0500

    chore: layout div

--- layouts/auth-layout.vue
+++ layouts/auth-layout.vue
@@ -1,17 +1,19 @@
 <template>
-  <div v-show="!isLoadingProfile && userProfile !== null">
-    <Sidebar />
-    <div class="pl-0 sm:pl-64">
-      <div class="px-8">
-        <slot />
+  <div>
+    <div v-show="!isLoadingProfile && userProfile !== null">
+      <Sidebar />
+      <div class="pl-0 sm:pl-64">
+        <div class="px-8">
+          <slot />
+        </div>
       </div>
     </div>
-  </div>
 
-  <div v-show="isLoadingProfile">
-    <SidebarLoader />
-    <div class="pl-0 sm:pl-64">
-      <div class="px-8"></div>
+    <div v-show="isLoadingProfile">
+      <SidebarLoader />
+      <div class="pl-0 sm:pl-64">
+        <div class="px-8"></div>
+      </div>
     </div>
   </div>
 </template>
