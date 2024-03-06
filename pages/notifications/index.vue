021f1ad58d4082b3393032f61d41d52a0183c2b4
waleedbinalim <waleedbinalim@gmail.com>
2024-03-06 15:07:00 +0500

    fix: app glitch when routed from notifications

--- pages/notifications/index.vue
+++ pages/notifications/index.vue
@@ -1,28 +1,30 @@
 <template>
-  <NuxtLayout :name="`auth-layout`">
-    <div
-      class="mt-8 rounded-lg bg-slate-50 p-4 dark:bg-slate-800 dark:text-white"
-    >
+  <div>
+    <NuxtLayout :name="`auth-layout`">
       <div
-        class="w-100 font-md border-b-2 border-slate-400 px-4 py-2 font-medium"
+        class="mt-8 rounded-lg bg-slate-50 p-4 dark:bg-slate-800 dark:text-white"
       >
-        <span class="mr-2 text-3xl"> 🖤 </span>
-        Numan likes your review on
-        <span class="cursor-pointer text-blue-500 underline">Ergo Proxy</span>
-      </div>
-      <div
-        class="w-100 font-md border-b-2 border-slate-400 px-4 py-2 font-medium"
-      >
-        <span class="mr-2 text-3xl"> 📩 </span>
+        <div
+          class="w-100 font-md border-b-2 border-slate-400 px-4 py-2 font-medium"
+        >
+          <span class="mr-2 text-3xl"> 🖤 </span>
+          Numan likes your review on
+          <span class="cursor-pointer text-blue-500 underline">Ergo Proxy</span>
+        </div>
+        <div
+          class="w-100 font-md border-b-2 border-slate-400 px-4 py-2 font-medium"
+        >
+          <span class="mr-2 text-3xl"> 📩 </span>
 
-        You recieved a new msg from Jonas
+          You recieved a new msg from Jonas
+        </div>
+        <div class="w-100 font-md px-4 py-2 font-medium">
+          <span class="mr-2 text-3xl"> 👋 </span>
+          You joined the platform on 21st January 2023
+        </div>
       </div>
-      <div class="w-100 font-md px-4 py-2 font-medium">
-        <span class="mr-2 text-3xl"> 👋 </span>
-        You joined the platform on 21st January 2023
-      </div>
-    </div>
-  </NuxtLayout>
+    </NuxtLayout>
+  </div>
 </template>
 
-<script setup></script>
+<script setup lang="ts"></script>
