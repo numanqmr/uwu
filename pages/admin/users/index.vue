0d649c2d65c5aed79b8687601a1cb4e1d5712f8f
waleedbinalim <waleedbinalim@gmail.com>
2024-03-24 22:39:04 +0500

    chore: minor comments added

--- pages/admin/users/index.vue
+++ pages/admin/users/index.vue
@@ -5,12 +5,6 @@
 
       <div class="text-2xl font-bold dark:text-white">Pending Users</div>
 
-      <!-- PRIME VUE TABLE -->
-      <!-- <AdminUsers /> -->
-
-      <!-- TANSTACK TABLE -->
-      <div class="pt-8" />
-      <AdminUsersTanstackTable />
       <div class="pt-8" />
 
       <AdminUsersCards />
